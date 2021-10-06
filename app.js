ec8462b5f172827d8b3f4490675bd0eba0a39c3b
healym3 <79485702+healym3@users.noreply.github.com>
2021-10-06 09:50:35 -0500

    debug with json data

--- app.js
+++ app.js
@@ -38,14 +38,16 @@ app.post("/", function(req, res){
   }
   const request = https.request(url, options, function(response){
     if (response.statusCode === 200){
-      res.sendFile(__dirname + "/success.html");
+      //res.sendFile(__dirname + "/success.html");
     } else {
-      res.sendFile(__dirname + "/failure.html");
+
+      //res.sendFile(__dirname + "/failure.html");
     }
     response.on("data", function(data){
       //console.log(data
       //console.log(JSON.stringify(data));
       console.log(JSON.parse(data));
+      res.send(JSON.parse(data));
     });
   });
   request.write(jsonData);
